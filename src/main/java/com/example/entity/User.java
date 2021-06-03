0f00ea973f15d3d654e0e8d43717281c0209f9ce
Gorshkov Sergey <Gsn82@mail.ru>
2021-06-03 22:12:35 +0500

    11

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -1,27 +1,52 @@
 package com.example.entity;
 
 import com.example.entity.enums.ERole;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+
+@Data
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(unique = true, updatable = false)
     private String username;
+    @Column(nullable = false)
     private String lastname;
+    @Column(unique = true)
     private String email;
+    @Column(columnDefinition = "text")
     private String bio;
+    @Column(length = 3000)
     private String password;
 
-    private Set<ERole> role = new HashSet<>();
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"))
+    private Set<ERole> roles = new HashSet<>();
+
+    @OneToMany(cascade =  CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createdDate;
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public User(){
+
+    }
 
     @PrePersist
     protected void onCreate(){
